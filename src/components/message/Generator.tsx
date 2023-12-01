1eeb9aa8cdc086c624bbd69f31e0d971510a27b9
Jiheon Park <jhpark.a@imagoworks.ai>
2023-12-01 18:45:14 +0900

    modify: change not supported conditions

--- src/components/message/Generator.tsx
+++ src/components/message/Generator.tsx
@@ -25,27 +25,22 @@ const Generator = () => {
         boxShadow: '0px -4px 8px rgba(0, 0, 0, 0.08)',
       }}
     >
-      {browserSupportsSpeechRecognition ? (
-        <Stack justifyContent="center" alignItems="center">
-          <Box p={'20px 15px'} height="70px">
-            {transcript}
-          </Box>
-          <Button
-            type="button"
-            onMouseDown={startListening}
-            onMouseUp={stopListening}
-            startIcon={listening ? <MicIcon /> : <MicOffIcon />}
-            size="large"
-            sx={{ minWidth: '135px', animation: listening ? 'wave 1.2s infinite' : '' }}
-          >
-            {listening ? '음성인식 중..' : '음성인식 시작'}
-          </Button>
-        </Stack>
-      ) : (
-        <Stack justifyContent="center" alignItems="center">
-          <Typography variant="h3">Not Supported Browser</Typography>
-        </Stack>
-      )}
+      <Stack justifyContent="center" alignItems="center">
+        <Box p={'20px 15px'} height="70px">
+          {transcript}
+        </Box>
+        <Button
+          type="button"
+          onMouseDown={startListening}
+          onMouseUp={stopListening}
+          startIcon={listening ? <MicIcon /> : <MicOffIcon />}
+          size="large"
+          sx={{ minWidth: '135px', animation: listening ? 'wave 1.2s infinite' : '' }}
+          disabled={!browserSupportsSpeechRecognition}
+        >
+          {browserSupportsSpeechRecognition ? (listening ? '음성인식 중..' : '음성인식 시작') : 'Not Supported Browser'}
+        </Button>
+      </Stack>
     </Stack>
   );
 };
